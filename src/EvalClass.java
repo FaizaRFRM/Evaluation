00f168f2f4582d61adad338744438a341932a86c
FaizaRFRM <alamani97668779@gmail.com>
2022-11-29 04:24:10 -0800

    create search of word from file

--- src/EvalClass.java
+++ src/EvalClass.java
@@ -1,11 +1,8 @@
-import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.Reader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,10 +12,7 @@ import java.util.Scanner;
 
 
 public class EvalClass {
-static void ext(){
-	    
-	    System.out.println("Exit");
-}
+
 	public static void main(String[] args)throws IOException 
 	{
 		 boolean ex=true;
@@ -32,7 +26,10 @@ static void ext(){
 	    System.out.println("2-Write response  a file");
 	    System.out.println("3-search from file");
 	    System.out.println("4-Exit");
-	    System.out.println("========================================================\n\n");
+	    System.out.println("========================================================\n");
+	    
+	    
+	    
 	    int option;
 	    option=sa.nextInt();
 	    switch(option) {
@@ -47,8 +44,11 @@ static void ext(){
 		    
 			String fileF= response.body();
 			System.out.println(fileF);
-			System.out.println("========================================================\n\n");
+			System.out.println("========================================================\n");
 	    	break;
+	    	
+	    	
+	    	
 	    case 2:
 	    	HttpClient client1 = HttpClient.newHttpClient();
 		    HttpRequest request1 = HttpRequest.newBuilder()
@@ -57,11 +57,10 @@ static void ext(){
 		    HttpResponse<String> response1 = client1.send(request1,HttpResponse.BodyHandlers.ofString());
 		    
 		    
-			String fileA= response1.body();
-//			System.out.println(fileA);
+			String file= response1.body();
 			try {
 		    	FileWriter writerFile= new FileWriter("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
-		    	writerFile.write(fileA);
+		    	writerFile.write(file);
 		    	writerFile.close();
 		    }
 		    catch(IOException e) {
@@ -69,67 +68,179 @@ static void ext(){
 		    }
 			System.out.println("JSON file was created it iclude:");
 			System.out.println("==================================");
-			System.out.println(fileA);
-	    	System.out.println("========================================================\n\n");
+			System.out.println(file);
+	    	System.out.println("========================================================\n");
 
 	    	try {
-		    File myObj = new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
-   	     Scanner myReader = new Scanner(myObj);
+		    File read = new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
+   	     Scanner Reader = new Scanner(read);
 	    	System.out.println("========================================================");
    	  System.out.println("Reading file");
   	System.out.println("========================================================");
-   	     while (myReader.hasNextLine()) {
-   	       String data = myReader.nextLine();
+   	     while (Reader.hasNextLine()) {
+   	       String details = Reader.nextLine();
    	   
-   	       System.out.println(data);
+   	       System.out.println(details);
    	     }
-   	     myReader.close();
+   	     Reader.close();
    	   } catch (FileNotFoundException e) {
    	     e.printStackTrace();
    	   }
-	    	System.out.println("========================================================\n\n");
+	    	System.out.println("========================================================\n");
 	    	break;
+	    	
+	    	
 	    case 3:
 	    	
-	    	File file = new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
-	    	System.out.println(" enter the content you looking for");
-	    	
-	        Scanner word = new Scanner(System.in);
-	        String[] name = new String [word.nextInt()]; 
-	        word.nextLine();
-	       Scanner scanner;
-	       try {
-	           scanner = new Scanner(file).useDelimiter( ",");
+	    	 File file1 = new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
 
-	           while (scanner.hasNext()) {
+	         Scanner sc = new Scanner(System.in);
+
+	        System.out.println("enter words you want to search");
+	        String name = sc.next();
+	        System.out.println("========================================================");
+	        Scanner scanner;
+	        try {
+	            scanner = new Scanner(file1).useDelimiter( ",");
+
+	            while (scanner.hasNext()) {
+	                final String lineFromFile = scanner.nextLine();
+	                if (lineFromFile.contains(name)) {
+	                    System.out.println("It is Available " + name);
+	                    System.out.println("***********************");
+	                    break;
+	                }
+	            }
+	        } catch (IOException e) {
+	            System.out.println("Not available in the source: " + file1.toString());
+	        }
+	        
+	        
+	        
+//**********************************************************************	        
+//	    	    File file1 = new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
+//	    	    Scanner sc = new Scanner(System.in);
+//	    	    System.out.println("enter words you want to search");
+//	    	    String name = sc.next();
+//	    	    System.out.println("========================================================");
+////	    	    for(int i=0;i<name.length();i++) {
+//	    	    
+//	    	    Scanner scanner;
+//	    	    try {
+//	    	        scanner = new Scanner(file1).useDelimiter( ",");
+//	    	    
+//	    	        while (scanner.hasNext()) {
+//	    	            final String lineFromFile = scanner.nextLine();
+//	    	            if (lineFromFile.contains(name)) {
+//	    	                System.out.println("It is Available" + "   "+name);
+//	    	                System.out.println("***********************");
+//	    	                break;
+//	    	                
+//	    	            }
+////	    	            else {
+////	    	            	System.out.println("Not available in the source:" + "   "+file1.toString());
+////	    	    	        System.out.println("***********************");
+////	    	    	        break;
+//	    	            }
+////	    	    }
+//	    	    } catch (IOException e) {
+//	    	    	System.out.println("Not available in the source:" + "   "+file1.toString());
+//	    	    }
+	    	    
+//****************************** ********************************************
+//	    	File file = new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
+//	    	System.out.println(" enter words you want to search");
+//	    	
+//	        Scanner word = new Scanner(System.in);
+//	        String[] name = new String [word.nextInt()]; 
+//	        word.nextLine();
+//	       Scanner scanner;
+//	       try {
+//	           scanner = new Scanner(file).useDelimiter( ",");
+//
+//	           while (scanner.hasNext()) {
 //	               final String lineFromFile = scanner.nextLine();
 //	               lineFromFile.contains(name);
-	                   System.out.println("I found " + name);
-	                   break;
-	           }
-	       } catch (IOException e) {
-	           System.out.println(" cannot write to file " + file.toString());
-	       }
+//	                   System.out.println("I found " + name);
+//	                   break;
+//	           }
+//	       } catch (IOException e) {
+//	           System.out.println(" cannot write to file " + file.toString());
+//	       }
+	       
+//********************************************************************************	    	
+	    	
+//	    	File file = new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
+//	    	System.out.println(" enter words you want to search");
+//	    	
+//	        Scanner sc = new Scanner(System.in);
+//	        String[] name = new String [name.nextInt()]; 
+//	        name.nextLine();
+////	    	String[] words=null;
+//		      
+//	       Scanner scanner;
+//	       FileReader fileReader = new FileReader(file);
+//	       try {
+//	           scanner = new Scanner(file).useDelimiter( ",");
+//
+//	           while (scanner.hasNext()) {
+//	               final String lineFromFile = scanner.nextLine();
+//	               lineFromFile.contains(name);
+//	                   System.out.println("I found " + name);
+//	                   break;
+//	           }
+//	       } catch (IOException e) {
+//	           System.out.println(" cannot write to file " + file.toString());
+//	       }
+//	       fileReader.close();
 	       
 	       
+//******************************************************************************	       
+	     	
+//	       File f1=new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt"); //Creation of File Descriptor for input file
+//	       String[] words=null;  //Intialize the word Array
+//	       FileReader filereader = new FileReader(f1);  //Creation of File Reader object
+//	       BufferedReader br = new BufferedReader(words); //Creation of BufferedReader object
+//	       String s;     
+//	       String input="Java";   // Input word to be searched
+//	       int count=0;   //Intialize the word to zero
+//	       while((s=bufferReader.readLine())!=null)   //Reading Content from the file
+//	       {
+//	          words=s.split(" ");  //Split the word using space
+//	           for (String word : words) 
+//	           {
+//	                  if (word.equals(input))   //Search for the given word
+//	                  {
+//	                    count++;    //If Present increase the count by one
+//	                  }
+//	           }
+//	       }
+//	       if(count!=0)  //Check for count not equal to zero
+//	       {
+//	          System.out.println("The given word is present for "+count+ " Times in the file");
+//	       }
+//	       else
+//	       {
+//	          System.out.println("The given word is not present in the file");
+//	       }
+//	       
+//	          fr.close();
+//	*************************************************************************       
 	       
-	       
-	       
-	       
-	       
+	    	    
 //	       File file = new File("C:\\Users\\user16\\eclipse-workspace\\taskFaiza\\MYAPI.txt");
 //
-//	       Scanner kb = new Scanner(System.in);
+//	       Scanner word = new Scanner(System.in);
 //
 //	      System.out.println(" enter the content you looking for");
-//	      String name = kb.next();
+//	      String string = word.next();
 //	      Scanner scanner;
 //	      try {
 //	          scanner = new Scanner(file).useDelimiter( ",");
 //
 //	          while (scanner.hasNext()) {
 //	              final String lineFromFile = scanner.nextLine();
-//	              if (lineFromFile.contains(name)) {
+//	              if (lineFromFile.contains(string)) {
 //	                  // a match!
 //	                  System.out.println("I found " + name);
 //	                  break;
@@ -138,10 +249,12 @@ static void ext(){
 //	      } catch (IOException e) {
 //	          System.out.println(" cannot write to file " + file.toString());
 //	      }
+//****************************************************
 	    	break;
 	    case 4:
-	    	ext();
-            ex = false;
+            System.out.println("program is Exit");
+
+	    	System.exit(0);
 	    	break;
 	    }
 		}
